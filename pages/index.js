03a0db89b85f0a06e12812ee65a8c568eb6435b4
Alaksandu <alexandre.nike10@hotmail.com>
2021-02-04 18:53:08 -0300

    test, envio do Layout

---
+++ pages/index.js
@@ -0,0 +1,61 @@
+import styled from 'styled-components'
+import db from '../db.json'
+import Widget from '../source/components/Widgets'
+import Footer from '../source/components/Footer'
+import GitHubCorner from '../source/components/GitHubCorner'
+import QuizBackground from '../source/components/QuizBackground'
+
+// const Title = styled.h1`
+//   font-size: 50px;
+//   color: ${({ theme }) => theme.colors.primary};
+// `
+
+// function Title(props){
+//   return <h1>{props.children}</h1>
+// }
+
+// const BackgroundImage = styled.div`
+//     background-image: url(${db.bg});
+//     flex: 1;
+//     background-size: cover;
+//     background-position: center;
+// `;
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  } 
+`;
+
+
+export default function Home() {
+  return (
+      <QuizBackground>
+        <QuizContainer>
+          <Widget>
+            <Widget.Header>
+              <h1> Primeira Caixa</h1>
+            </Widget.Header>
+            <Widget.Content>
+              <p>Lorem ipsum mussum e tals...</p>
+            </Widget.Content>
+          </Widget>
+
+          <Widget>
+            <Widget.Content>
+              <h1>Segunda Caixa</h1>
+
+              <p>Lorem ipsum mussum e tals...</p>
+            </Widget.Content>
+          </Widget>
+          <Footer />
+        </QuizContainer>
+          <GitHubCorner projectUrl="https://github.com/Alaksandu/QuizBatmanImersaoAlura"/>
+      </QuizBackground>
+    )
+}
